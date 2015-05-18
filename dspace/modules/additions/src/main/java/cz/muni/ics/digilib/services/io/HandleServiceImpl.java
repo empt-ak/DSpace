6d229d046f1952757a4581fdd9012fcc4825e3cc
Dominik Szalai <emptulik@gmail.com>
2015-05-18 10:21:35 +0200

    moved handleservice into digilib package, as dml uses different handle generation and mechanism is not general for all

--- dspace/modules/additions/src/main/java/cz/muni/ics/digilib/services/io/HandleServiceImpl.java
+++ dspace/modules/additions/src/main/java/cz/muni/ics/digilib/services/io/HandleServiceImpl.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cz.muni.ics.dspace5.impl.io;
+package cz.muni.ics.digilib.services.io;
 
 import cz.muni.ics.dspace5.api.HandleService;
 import cz.muni.ics.dspace5.impl.ContextWrapper;
+import cz.muni.ics.dspace5.impl.io.FolderProvider;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
