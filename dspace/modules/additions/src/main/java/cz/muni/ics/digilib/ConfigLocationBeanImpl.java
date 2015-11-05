7e6ee27b981c7f3dc54b0011a18e040e57bb321c
Dominik Szalai <emptulik@gmail.com>
2015-11-05 11:59:20 +0100

    fixed wrong path

--- dspace/modules/additions/src/main/java/cz/muni/ics/digilib/ConfigLocationBeanImpl.java
+++ dspace/modules/additions/src/main/java/cz/muni/ics/digilib/ConfigLocationBeanImpl.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 public class ConfigLocationBeanImpl implements ConfigLocationBean
 {
 
-    private static final Path configFileLocation = Paths.get("/opt/oktavo5/config");
+    private static final Path configFileLocation = Paths.get("/opt/digilib5/config");
     @Override
     public Path getLocation()
     {
